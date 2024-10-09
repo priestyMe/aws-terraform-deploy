4fbbf483f6ef2163ad9ba7780b283678c7988d59
Olanrewaju Oyekale <oyekaleop@gmail.com>
2024-10-10 03:12:30 +1030

    Implement ec2 deployment with aws auto-scaling group

--- terraform-modules/ec2-instances/outputs.tf
+++ terraform-modules/ec2-instances/outputs.tf
@@ -6,17 +6,7 @@ data "aws_instances" "asg_instances" {
   }
 }
 
-# Output the public IPs of the instances in the Auto Scaling Group
-output "instance_public_ips" {
-  value = data.aws_instances.asg_instances.*.public_ip
-}
-
 # Output the instance IDs of the instances in the Auto Scaling Group
 output "instance_ids" {
   value = data.aws_instances.asg_instances.*.id
-}
-
-# Output the names of the instances
-output "instance_names" {
-  value = [for instance in data.aws_instances.asg_instances : instance.tags["Name"]]
 }
